ab8e18f1ded49045dca72eac4f73adc7b558d9ac
sangwoo <lesw1216@gmail.com>
2025-10-03 01:30:53 +0900

    refactor: User 엔티티 수정

--- src/main/java/com/halo/core_bridge/api/users/model/entity/User.java
+++ src/main/java/com/halo/core_bridge/api/users/model/entity/User.java
@@ -21,6 +21,7 @@ public class User extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
     private String email;
     private String password;
     private String birth;
