ffb11db8a6b4de3acdbadab929cbfea129147f8a
sangwoo <lesw1216@gmail.com>
2025-10-03 20:44:11 +0900

    fix: User 엔티티 수정, Gender 추가
    User 엔티티 컬럼 제약사항 추가
    Gender Enum 추가

--- src/main/java/com/halo/core_bridge/api/users/model/entity/User.java
+++ src/main/java/com/halo/core_bridge/api/users/model/entity/User.java
@@ -1,31 +1,49 @@
 package com.halo.core_bridge.api.users.model.entity;
 
-import com.halo.core_bridge.common.model.BaseEntity;
 import com.halo.core_bridge.api.board.model.entity.board;
+import com.halo.core_bridge.api.users.model.Gender;
+import com.halo.core_bridge.common.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_USER_EMAIL", columnNames = "email")
+        }
+)
 public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
     private String password;
-    private String birth;
-    private String gender;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false)
     private String phone;
 
     @OneToMany(mappedBy = "user")
