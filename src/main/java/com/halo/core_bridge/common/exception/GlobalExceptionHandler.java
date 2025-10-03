1f7277b4dbd7dec2a8b2fb9e14544cd731dae895
sangwoo <lesw1216@gmail.com>
2025-10-03 21:44:41 +0900

    feat: 필드 값 검증 예외 처리 관련 추가

--- src/main/java/com/halo/core_bridge/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/halo/core_bridge/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.halo.core_bridge.common.exception;
 
 import com.halo.core_bridge.common.model.BaseResponse;
+import com.halo.core_bridge.common.model.BaseResponseStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -28,6 +34,21 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(httpStatusCodeMapper(e.getStatus().getCode()))
                 .body(
                         BaseResponse.error(e.getStatus())
+                );
+    }
+
+    // @Valid 검증 실패 처리
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<Object>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        Map<String, Object> errors = new HashMap<>();
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(httpStatusCodeMapper(e.getStatusCode().value()))
+                .body(
+                        BaseResponse.error(BaseResponseStatus.FIELD_VALIDATE_ERROR, errors)
                 );
     }
 }
