d44d5c2c107b189ca0bd08766d85183f6a27b7e7
sangwoo <lesw1216@gmail.com>
2025-10-04 20:39:21 +0900

    test: 이메일 중복 검사 테스트

--- src/test/java/com/halo/core_bridge/api/users/service/UserServiceTest.java
+++ src/test/java/com/halo/core_bridge/api/users/service/UserServiceTest.java
@@ -6,12 +6,11 @@ import com.halo.core_bridge.api.users.model.entity.User;
 import com.halo.core_bridge.api.users.model.entity.UserRole;
 import com.halo.core_bridge.api.users.repository.UserRepository;
 import com.halo.core_bridge.common.exception.BaseException;
-import org.assertj.core.api.Assertions;
+import com.halo.core_bridge.common.model.BaseResponseStatus;
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,8 +19,11 @@ import org.springframework.dao.DataIntegrityViolationException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
@@ -52,7 +54,7 @@ class UserServiceTest {
                 )
                 .build();
 
-        BDDMockito.given(userRepository.save(any(User.class))).willReturn(willReturnUser);
+        given(userRepository.save(any(User.class))).willReturn(willReturnUser);
 
         UserDto.Create createUser = UserDto.Create.builder()
                 .email("test01@test.com")
@@ -67,7 +69,7 @@ class UserServiceTest {
         Long savedKey = userService.save(createUser);
 
         // then
-        Assertions.assertThat(willReturnUser.getId()).isEqualTo(savedKey);
+        assertThat(willReturnUser.getId()).isEqualTo(savedKey);
         then(userRepository).should(times(1)).save(any(User.class));
     }
 
@@ -85,7 +87,7 @@ class UserServiceTest {
         DataIntegrityViolationException springEx =
                 new DataIntegrityViolationException("무결성 위반", hibernateEx);
 
-        BDDMockito.given(userRepository.save(any(User.class))).willThrow(springEx);
+        given(userRepository.save(any(User.class))).willThrow(springEx);
 
         UserDto.Create createUser = UserDto.Create.builder()
                 .email("test01@test.com")
@@ -99,5 +101,28 @@ class UserServiceTest {
         // when
         // then
         assertThrows(BaseException.class, () -> userService.save(createUser));
+    }
+
+    @Test
+    @DisplayName("이메일 중복 예외 발생")
+    void existEmail() {
+        // given
+        given(userRepository.existsByEmail(any(String.class))).willReturn(true);
+
+        // when
+        // then
+        BaseException exception = assertThrows(BaseException.class, () -> userService.existByEmail("test@test.com"));
+        assertThat(exception.getStatus()).isEqualTo(BaseResponseStatus.DUPLICATE_USER_EMAIL);
+    }
+
+    @Test
+    @DisplayName("이메일 중복 검사 성공")
+    void noExistEmail() {
+        // given
+        given(userRepository.existsByEmail(any(String.class))).willReturn(false);
+
+        // when
+        // then
+        assertDoesNotThrow(() -> userService.existByEmail("test@test.com"));
     }
 }
