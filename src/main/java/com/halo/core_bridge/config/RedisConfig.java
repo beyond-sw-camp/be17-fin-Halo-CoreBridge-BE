891414365ef743187f9712448ebb0add8b651d78
sangwoo <lesw1216@gmail.com>
2025-10-12 23:32:00 +0900

    feat: RefreshToken을 저장하는 RedisTemplate 등록

--- src/main/java/com/halo/core_bridge/config/RedisConfig.java
+++ src/main/java/com/halo/core_bridge/config/RedisConfig.java
@@ -1,13 +1,15 @@
 package com.halo.core_bridge.config;
 
+import com.halo.core_bridge.api.token.refresh.model.dto.RefreshTokenDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -28,5 +30,20 @@ public class RedisConfig {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
         stringRedisTemplate.setConnectionFactory(connectionFactory);
         return stringRedisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, RefreshTokenDto.Meta> refreshTokenRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, RefreshTokenDto.Meta> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
 }
