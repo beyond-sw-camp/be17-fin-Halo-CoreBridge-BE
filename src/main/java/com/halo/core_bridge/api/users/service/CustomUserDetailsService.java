021bfd02252fa623e16d6cb2183d5ebc2e628006
sangwoo <lesw1216@gmail.com>
2025-10-12 23:35:50 +0900

    feat: 로그인 기능 구현
    1. 로그인 기능 구현
    2. JWT 토큰 생성 후 쿠키에 담아 응답
    3. RefreshToken 생성 후 쿠키에 담아 응답

---
+++ src/main/java/com/halo/core_bridge/api/users/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.halo.core_bridge.api.users.service;
+
+import com.halo.core_bridge.api.users.model.dto.UserDto;
+import com.halo.core_bridge.api.users.model.entity.User;
+import com.halo.core_bridge.api.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> findUser = userRepository.findByEmail(username);
+
+        if (findUser.isPresent()) {
+
+            User user = findUser.get();
+
+            return UserDto.Auth.builder()
+                    .id(user.getId())
+                    .email(user.getEmail())
+                    .password(user.getPassword())
+                    .name(user.getName())
+                    .role(user.getUserRole().getCode())
+                    .build();
+        }
+
+        return null;
+    }
+}
