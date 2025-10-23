5a962791fe6d7f78bfab4b3f5d85bd6beba2b551
sangwoo <lesw1216@gmail.com>
2025-10-24 02:12:42 +0900

    fix: InterviewRepositor 수정
    CrudRepository 상속을 JpaRepository로 변경

--- src/main/java/com/halo/core_bridge/api/interview/repository/InterviewRepository.java
+++ src/main/java/com/halo/core_bridge/api/interview/repository/InterviewRepository.java
@@ -1,7 +1,7 @@
 package com.halo.core_bridge.api.interview.repository;
 
 import com.halo.core_bridge.api.interview.model.entity.Interview;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InterviewRepository extends CrudRepository<Interview, Long> {
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
 }
