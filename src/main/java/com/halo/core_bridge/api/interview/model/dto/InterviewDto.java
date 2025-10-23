3acdbc14a3eed6c609d08edf1e43b2ae7a3548b9
sangwoo <lesw1216@gmail.com>
2025-10-23 20:39:52 +0900

    feat: Interview Create DTO 생성

---
+++ src/main/java/com/halo/core_bridge/api/interview/model/dto/InterviewDto.java
@@ -0,0 +1,42 @@
+package com.halo.core_bridge.api.interview.model.dto;
+
+import com.halo.core_bridge.api.interview.model.entity.Interview;
+import com.halo.core_bridge.api.interview.model.entity.Room;
+import com.halo.core_bridge.api.interview.model.enums.InterviewStatus;
+import com.halo.core_bridge.api.jobposting.model.entity.RecruitProcess;
+import com.halo.core_bridge.api.resume.model.entity.Resume;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+public class InterviewDto {
+
+    @Getter
+    public static class Create {
+
+        private LocalDateTime startDateTime;
+        private int duration;
+        private InterviewStatus status;
+        private String description;
+        private Long resumeId;
+        private Long roomId;
+        private Long recruiterProcessId;
+
+        public Interview toEntity() {
+            return Interview.builder()
+                    .duration(this.duration)
+                    .status(this.status)
+                    .description(this.description)
+                    .resume(
+                            Resume.builder().id(resumeId).build()
+                    )
+                    .room(
+                            Room.builder().id(roomId).build()
+                    )
+                    .recruitProcess(
+                            RecruitProcess.builder().id(recruiterProcessId).build()
+                    )
+                    .build();
+        }
+    }
+}
