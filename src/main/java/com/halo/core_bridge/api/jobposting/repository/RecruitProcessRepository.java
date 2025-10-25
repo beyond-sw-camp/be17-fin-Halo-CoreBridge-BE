81eabbc8a14f9ee2b570a0ce1c988b02e599f42e
염준선 <wnstjs1031@naver.com>
2025-10-25 12:48:07 +0900

    fix: 채용프로세스 레포지토리 추가

---
+++ src/main/java/com/halo/core_bridge/api/jobposting/repository/RecruitProcessRepository.java
@@ -0,0 +1,7 @@
+package com.halo.core_bridge.api.jobposting.repository;
+
+import com.halo.core_bridge.api.jobposting.model.entity.RecruitProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitProcessRepository extends JpaRepository<RecruitProcess, Long> {
+}
