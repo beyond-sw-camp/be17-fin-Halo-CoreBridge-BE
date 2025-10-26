e3443f3017d6233188586962f9449b768d139da3
sangwoo <lesw1216@gmail.com>
2025-10-27 03:15:42 +0900

    refactor: 새로운 채용 프로세스 저장 후 응답에 채용 프로세스 전체 목록을 저장하도록 수정

--- src/main/java/com/halo/core_bridge/api/jobposting/controller/RecruitProcessController.java
+++ src/main/java/com/halo/core_bridge/api/jobposting/controller/RecruitProcessController.java
@@ -1,15 +1,13 @@
 package com.halo.core_bridge.api.jobposting.controller;
 
-import com.halo.core_bridge.api.jobposting.model.dto.RecruitProcessDto;
 import com.halo.core_bridge.api.jobposting.service.RecruitProcessService;
 import com.halo.core_bridge.common.model.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import static com.halo.core_bridge.api.jobposting.model.dto.RecruitProcessDto.*;
 
 @RestController
 @RequestMapping("/api/recruiter/processes")
@@ -19,25 +17,28 @@ public class RecruitProcessController {
     private final RecruitProcessService recruitProcessService;
 
     @PostMapping
-    public ResponseEntity<BaseResponse<Object>> createRecruitProcess(@RequestBody RecruitProcessDto.Create createRecruitProcessDto) {
+    public ResponseEntity<BaseResponse<recruitProcesses>> createRecruitProcess(@RequestBody Create createRecruitProcess) {
 
-        recruitProcessService.add(createRecruitProcessDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success("채용 프로세스 추가 완료"));
+        recruitProcessService.add(createRecruitProcess);
 
+        recruitProcesses recruitProcesses =
+                recruitProcessService.findAllRecruitProcessesByJobPostingId(createRecruitProcess.getJobPostingId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success(recruitProcesses));
     }
 
     @GetMapping
-    public ResponseEntity<BaseResponse<RecruitProcessDto.recruitProcesses>> getRecruitProcesses(@RequestParam("recruit") Long jobPostingId) {
+    public ResponseEntity<BaseResponse<recruitProcesses>> getRecruitProcesses(@RequestParam("recruit") Long jobPostingId) {
 
-        RecruitProcessDto.recruitProcesses findRecruitProcesses = recruitProcessService.findAllRecruitProcessesByJobPostingId(jobPostingId);
+        recruitProcesses findRecruitProcesses = recruitProcessService.findAllRecruitProcessesByJobPostingId(jobPostingId);
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.success(findRecruitProcesses));
     }
 
     @PatchMapping
-    public ResponseEntity<BaseResponse<Object>> changeProcessOrder(@RequestBody RecruitProcessDto.ChangeOrder changeOrder) {
+    public ResponseEntity<BaseResponse<Object>> changeProcessOrder(@RequestBody ChangeOrder changeOrder) {
 
         recruitProcessService.changeOrder(changeOrder);
-        RecruitProcessDto.recruitProcesses recruitProcesses = recruitProcessService.findAllRecruitProcessesByJobPostingId(changeOrder.getJobPostingId());
+        recruitProcesses recruitProcesses = recruitProcessService.findAllRecruitProcessesByJobPostingId(changeOrder.getJobPostingId());
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.success(recruitProcesses));
     }
