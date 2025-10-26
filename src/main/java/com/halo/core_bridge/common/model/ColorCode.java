9665a8291823afdf3fa7234043f2b8486e116830
sangwoo <lesw1216@gmail.com>
2025-10-26 23:10:17 +0900

    refactor: ColorCode 수정

--- src/main/java/com/halo/core_bridge/common/model/ColorCode.java
+++ src/main/java/com/halo/core_bridge/common/model/ColorCode.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum ColorCode {
-    BLUE("파랑", "blue-300"),
-    RED("빨강", "red-300"),
-    ORANGE("주황", "orange-300"),
-    PURPLE("보라", "purple-300"),
-    pink("분홍", "pink-300");
+
+    BLUE("파랑", "blue-500"),
+    RED("빨강", "red-500"),
+    ORANGE("주황", "orange-500"),
+    PURPLE("보라", "purple-500"),
+    PINK("분홍", "pink-500");
 
     private final String colorName;
     private final String colorCode;
