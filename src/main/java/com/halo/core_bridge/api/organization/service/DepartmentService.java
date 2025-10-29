ea1e156dd8001c053fd18ca6ce2767bf2758e4d4
염준선 <wnstjs1031@naver.com>
2025-10-29 18:00:23 +0900

    feat: 부서조회기능 구현
    - 부서(department)조회 기능 구현

--- src/main/java/com/halo/core_bridge/api/organization/service/DepartmentService.java
+++ src/main/java/com/halo/core_bridge/api/organization/service/DepartmentService.java
@@ -5,7 +5,6 @@ import com.halo.core_bridge.api.organization.model.entity.Department;
 import com.halo.core_bridge.api.organization.repository.DepartmentRepository;
 import com.halo.core_bridge.common.exception.BaseException;
 import com.halo.core_bridge.common.model.BaseResponseStatus;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +17,10 @@ public class DepartmentService {
     private final DepartmentRepository departmentRepository;
 
     //부서 조회
-    public Department getById(Long id) {
-        return departmentRepository.findById(id)
-                .orElseThrow(() -> BaseException.from(BaseResponseStatus.DEPARTMENT_NOT_FOUND));
-    }
+//    public Department getById(Long id) {
+//        return departmentRepository.findById(id)
+//                .orElseThrow(() -> BaseException.from(BaseResponseStatus.DEPARTMENT_NOT_FOUND));
+//    }
 
     //전체 조회
     public List<DepartmentDto.Read> getAllDepartments() {
