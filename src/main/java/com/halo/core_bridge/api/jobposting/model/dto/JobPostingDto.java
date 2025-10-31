a982af8c651d4b7cb77b35d48f5adf016c780e6b
염준선 <wnstjs1031@naver.com>
2025-10-31 11:30:32 +0900

    feat: 채용공고 dto에 coverLetterTitleRequest dto를 담은 리스트 필드(coverLetterTitles) 추가

--- src/main/java/com/halo/core_bridge/api/jobposting/model/dto/JobPostingDto.java
+++ src/main/java/com/halo/core_bridge/api/jobposting/model/dto/JobPostingDto.java
@@ -1,6 +1,7 @@
 package com.halo.core_bridge.api.jobposting.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.halo.core_bridge.api.coverLetterTitle.model.dto.CoverLetterTitleDto;
 import com.halo.core_bridge.api.jobposting.model.entity.*;
 import com.halo.core_bridge.api.organization.model.entity.Department;
 import com.halo.core_bridge.api.users.model.entity.User;
@@ -100,6 +101,10 @@ public class JobPostingDto {
         @NotEmpty(message = "채용 프로세스는 최소 1개 이상 입력해야 합니다.")
         @Valid
         private List<RecruitProcessDto.Create> recruitProcess;
+
+        @NotEmpty(message = "질문 항목은 최소 1개 이상 입력해야 합니다.")
+        @Valid
+        private List<CoverLetterTitleDto.CoverLetterTitleRequest> coverLetterTitles;
 
         // 급여
         @NotNull(message = "급여 형태는 필수 입력값입니다.")
