65b609db2bbd20729b7f67afd6e4f7d1f918ddc2
sangwoo <lesw1216@gmail.com>
2025-11-02 20:01:20 +0900

    chore: 부서 API Swagger 수정

--- src/main/java/com/halo/core_bridge/api/organization/controller/DepartmentController.java
+++ src/main/java/com/halo/core_bridge/api/organization/controller/DepartmentController.java
@@ -1,8 +1,15 @@
 package com.halo.core_bridge.api.organization.controller;
 
+import com.halo.core_bridge.api.organization.contents.SwaggerDepartmentContents;
 import com.halo.core_bridge.api.organization.model.dto.DepartmentDto;
 import com.halo.core_bridge.api.organization.service.DepartmentService;
 import com.halo.core_bridge.common.model.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,11 +23,16 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/department")
-@Tag(name = "부서관련 API", description = "부서관련 CRUD API")
+@Tag(name = "부서", description = "부서 조회 API")
 public class DepartmentController {
     private final DepartmentService departmentService;
 
-    //부서 조회
+    @Operation(summary = "부서 목록 조회", description = "모든 부서 목록을 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = BaseResponse.class),
+                            examples = @ExampleObject(value = SwaggerDepartmentContents.GET_DEPARTMENT_LIST_RESPONSE)))
+    })
     @GetMapping
     public ResponseEntity<BaseResponse<List<DepartmentDto.Read>>> getAllDepartments() {
         List<DepartmentDto.Read> allDepartments = departmentService.getAllDepartments();
