4be57c48a9a540c6eb5fbb473d3b787b0cd480d9
sangwoo <lesw1216@gmail.com>
2025-11-06 11:28:15 +0900

    refactor: 면접 관련 응답 메시지 추가

--- src/main/java/com/halo/core_bridge/common/model/BaseResponseStatus.java
+++ src/main/java/com/halo/core_bridge/common/model/BaseResponseStatus.java
@@ -61,6 +61,7 @@ public enum BaseResponseStatus {
      * 60000 : Server 오류
      */
     SERVER_ERROR(false, 60001, "서버와의 연결에 실패하였습니다."),
+    GLOBAL_ERROR(false, 60002, "올바르지 않은 요청입니다. 로그를 확인해주세요."),
 
 
     /**
@@ -83,7 +84,10 @@ public enum BaseResponseStatus {
     SCHEDULE_SHARE_NOT_FOUND(false, 73001, "존재하지 않는 공유 정보입니다."),
 
     RECRUIT_PROCESS_NOT_FOUND(false, 74001, "해당 공고에는 채용 프로세스가 존재하지 않습니다."),
-    RECRUIT_PROCESS_CANT_DELETE(false, 74002, "채용 프로세스에 지원자가 존재하면 삭제 할 수 없습니다.");
+    RECRUIT_PROCESS_CANT_DELETE(false, 74002, "채용 프로세스에 지원자가 존재하면 삭제 할 수 없습니다."),
+
+    // 면접
+    DUPLICATE_INTERVIEW_ROOM(false, 75000, "면접 장소는 중복 될 수 없습니다.");
 
     private final boolean isSuccess;
     private final int code;
