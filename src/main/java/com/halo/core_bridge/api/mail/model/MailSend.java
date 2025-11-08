cf30f7d2e3dc6e7212c05f306015490fe1399c1c
sangwoo <lesw1216@gmail.com>
2025-11-09 00:15:40 +0900

    refactor: MailSend 수정
    비밀번호 재설정 이메일 제목 수정

--- src/main/java/com/halo/core_bridge/api/mail/model/MailSend.java
+++ src/main/java/com/halo/core_bridge/api/mail/model/MailSend.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MailSend {
     AUTH_CODE_MAIL("[CoreBridge] 안녕하세요. 요청하신 인증번호를 보내드립니다.", "AUTH_CODE_MAIL:"),
-    PASSWORD_RESET_MAIL("[CoreBridge] 안녕하세요. 요청하신 비밀번호 재설정 링크를 보내드립니다.", "PASSWORD_RESET_MAIL:");
+    PASSWORD_RESET_MAIL("[CoreBridge] 안녕하세요. 비밀번호 재설정 링크를 보내드립니다.", "PASSWORD_RESET_MAIL:");
 
     private final String subject;
     private final String keyName;
