ef00dc74ef12471017f43576553245a1b7c549f1
sangwoo <lesw1216@gmail.com>
2025-11-09 05:35:51 +0900

    feat: 계정 목록 조회 API 구현

--- src/main/java/com/halo/core_bridge/api/admin/controller/AdminUserController.java
+++ src/main/java/com/halo/core_bridge/api/admin/controller/AdminUserController.java
@@ -1,15 +1,14 @@
 package com.halo.core_bridge.api.admin.controller;
 
-import com.halo.core_bridge.api.admin.model.AdminDto;
 import com.halo.core_bridge.api.admin.service.AdminUserService;
 import com.halo.core_bridge.common.model.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.halo.core_bridge.api.admin.model.AdminDto.AccountList;
+import static com.halo.core_bridge.api.admin.model.AdminDto.UserCreate;
 
 @RestController
 @RequestMapping("/api/admin/users")
@@ -19,9 +18,18 @@ public class AdminUserController {
     private final AdminUserService adminUserService;
 
     @PostMapping
-    public ResponseEntity<BaseResponse<Object>> createUser(@RequestBody AdminDto.UserCreate create) {
+    public ResponseEntity<BaseResponse<Object>> createUser(@RequestBody UserCreate create) {
 
         adminUserService.save(create);
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success("계정이 추가되었습니다."));
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<AccountList>> getAccounts(@RequestParam(name = "type", required = false) String roleType,
+                                                                 @RequestParam(name = "page", defaultValue = "0") int page,
+                                                                 @RequestParam(name = "search", required = false) String keyword) {
+
+        AccountList findAccounts = adminUserService.findAccounts(roleType, page, keyword);
+        return ResponseEntity.ok(BaseResponse.success(findAccounts));
     }
 }
