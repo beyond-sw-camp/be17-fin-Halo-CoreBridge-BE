3c8012819948bdbae15dbc8244a8629622b64a8d
sangwoo <lesw1216@gmail.com>
2025-11-10 00:14:06 +0900

    feat: 면접 생성 관련 예외 메시지 추가

--- src/main/java/com/halo/core_bridge/common/model/BaseResponseStatus.java
+++ src/main/java/com/halo/core_bridge/common/model/BaseResponseStatus.java
@@ -91,6 +91,8 @@ public enum BaseResponseStatus {
     DUPLICATE_INTERVIEW_ROOM(false, 75000, "면접 장소는 중복 될 수 없습니다."),
     NOT_FOUND_INTERVIEW_ROOM(false, 75001, "면접 장소를 찾을 수 없습니다."),
     NOT_PROVIDED_CAPACITY_FOR_OFFLINE_ROOM(false, 75002, "오프라인 장소에는 수용인원이 필요합니다."),
+    ALREADY_SCHEDULED_INTERVIEW(false, 75003, "해당 지원자는 이미 면접을 예약하였습니다"),
+    RESUME_PROCESS_NOT_MATCH(false, 75004, "이력서와 채용 프로세스가 일치하지 않습니다"),
 
     // 사용자
     NOT_FOUND_USER_ROLE(false, 75002, "권한을 찾을 수 없습니다.");
