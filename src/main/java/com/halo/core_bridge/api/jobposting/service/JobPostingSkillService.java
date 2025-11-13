a4c8141bfea5f3a4e5eddd6763ec837c61dd923d
염준선 <wnstjs1031@naver.com>
2025-11-13 15:08:27 +0900

    fix: 기술스택 enum 처리

--- src/main/java/com/halo/core_bridge/api/jobposting/service/JobPostingSkillService.java
+++ src/main/java/com/halo/core_bridge/api/jobposting/service/JobPostingSkillService.java
@@ -1,10 +1,13 @@
 package com.halo.core_bridge.api.jobposting.service;
 
+import com.halo.core_bridge.api.jobposting.model.dto.TeckStackDto.TeckStackResponse;
 import com.halo.core_bridge.api.jobposting.model.entity.JobPosting;
 import com.halo.core_bridge.api.jobposting.model.entity.JobPostingSkill;
+import com.halo.core_bridge.api.jobposting.model.entity.TechStack;
 import com.halo.core_bridge.api.jobposting.repository.JobPostingSkillRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.*;
 
 @Service
@@ -22,5 +25,11 @@ public class JobPostingSkillService {
                 .toList();
 
         jobPostingSkillRepository.saveAll(jobPostingSkills);
+    }
+
+    // 현재 보유한 모든 기술스택 응답
+    public List<TeckStackResponse> getAllTechStacks() {
+        return Arrays.stream(TechStack.values()).map(ts -> new TeckStackResponse(ts.name(), ts.getLabel()))
+                .toList();
     }
 }
