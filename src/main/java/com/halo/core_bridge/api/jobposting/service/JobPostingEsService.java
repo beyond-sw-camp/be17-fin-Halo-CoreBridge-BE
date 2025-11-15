f1ff432997f4a9f8ded82e4ff9ac892543ae826a
sangwoo <lesw1216@gmail.com>
2025-11-15 14:52:38 +0900

    refactor: Elastic을 사용한 목록 조회 수정

--- src/main/java/com/halo/core_bridge/api/jobposting/service/JobPostingEsService.java
+++ src/main/java/com/halo/core_bridge/api/jobposting/service/JobPostingEsService.java
@@ -1,66 +1,46 @@
 package com.halo.core_bridge.api.jobposting.service;
 
-import com.halo.core_bridge.api.jobposting.model.document.JobPostingDoc;
-import com.halo.core_bridge.api.jobposting.model.document.RecruitProcessDoc;
+import com.halo.core_bridge.api.jobposting.model.document.JobPostingDocument;
 import com.halo.core_bridge.api.jobposting.model.dto.JobPostingDto;
-import com.halo.core_bridge.api.jobposting.model.entity.CareerType;
-import com.halo.core_bridge.api.jobposting.model.entity.EmploymentType;
-import com.halo.core_bridge.api.jobposting.repository.JobPostingEsRepository;
-import com.halo.core_bridge.api.jobposting.repository.RecruitProcessEsRepository;
+import com.halo.core_bridge.api.jobposting.repository.es.JobPostingSearchRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static com.halo.core_bridge.api.jobposting.model.dto.JobPostingDto.*;
+import static com.halo.core_bridge.api.jobposting.model.dto.JobPostingDto.JobPostingPage;
 
 @Service
 @RequiredArgsConstructor
 public class JobPostingEsService {
 
-    private final JobPostingEsRepository jobPostingESRepository;
-    private final RecruitProcessEsRepository recruitProcessESRepository;
+    private final JobPostingSearchRepository jobPostingSearchRepository;
 
-    public JobPostingListDto searchJobPostings(JobPostingDto.SearchQuery searchQuery) {
+    public JobPostingPage searchJobPostings(JobPostingDto.SearchQuery searchQuery) {
 
         PageRequest pageable = PageRequest.of(searchQuery.getPage(), 10, Sort.by("id").descending());
 
+        SearchHits<JobPostingDocument> results = jobPostingSearchRepository.search(searchQuery, pageable);
 
-        Page<JobPostingDoc> postings = jobPostingESRepository.findAllByTitle(searchQuery.getKeyword(), pageable);
-
-        List<JobPostingListResponseDto> resultList = postings.stream()
-                .map(posting -> {
-                    List<RecruitProcessDoc> processes = recruitProcessESRepository.findByJobPostingId(posting.getId());
-
-                    var processSummaries = processes.stream()
-                            .map(proc -> new JobPostingListResponseDto.ProcessSummary(
-                                    proc.getStageName(),
-                                    proc.getCount(),
-                                    proc.getOrderIndex()
-                            ))
-                            .collect(Collectors.toList());
-
-                    return JobPostingListResponseDto.builder()
-                            .id(posting.getId())
-                            .title(posting.getTitle())
-                            .departmentName(posting.getDepartmentName())
-                            .employmentType(EmploymentType.valueOf(posting.getEmploymentType()))
-                            .careerType(CareerType.valueOf(posting.getCareerType()))
-                            .summaryText(posting.getSummaryText())
-                            .status(posting.getStatus())
-                            .hireEndDate(posting.getHireEndDate().toLocalDate())
-                            .dday(posting.getDday())
-                            .applicantCount(posting.getApplicantCount())
-                            .progressPercent(posting.getProgressPercent())
-                            .processSummaries(processSummaries)
-                            .build();
-                })
+        List<JobPostingDocument> docs = results.getSearchHits()
+                .stream()
+                .map(SearchHit::getContent)
                 .toList();
 
-        return JobPostingListDto.from(resultList, postings.getNumber(), postings.getTotalPages(), postings.getTotalElements());
+        long totalElements = results.getTotalHits();
+        int currentPage = pageable.getPageNumber();
+        int pageSize = pageable.getPageSize();
+        int totalPages = (int) Math.ceil((double) totalElements / pageSize);
+
+        return JobPostingPage.fromJobPostingDocument(
+                docs,
+                currentPage,
+                totalPages,
+                totalElements
+        );
     }
 }
