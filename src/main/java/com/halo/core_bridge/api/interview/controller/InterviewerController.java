ef0b6847d2ff7160bb1affb4adcfc06379fbbaf8
sangwoo <lesw1216@gmail.com>
2025-11-16 15:03:24 +0900

    refactor: 채용 공고 생성 시 면접관 조회 API 패키지 이동

--- src/main/java/com/halo/core_bridge/api/interview/controller/InterviewerController.java
+++ src/main/java/com/halo/core_bridge/api/interview/controller/InterviewerController.java
@@ -1,7 +1,9 @@
 package com.halo.core_bridge.api.interview.controller;
 
+import com.halo.core_bridge.api.admin.model.AdminDto;
 import com.halo.core_bridge.api.interview.model.dto.InterviewerDto;
 import com.halo.core_bridge.api.interview.service.InterviewerService;
+import com.halo.core_bridge.api.users.service.UserService;
 import com.halo.core_bridge.common.model.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class InterviewerController {
 
     private final InterviewerService interviewerService;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<BaseResponse<Object>> getInterviewsByJobPosting(@RequestParam Long jobPostingId) {
 
         InterviewerDto.InterviewerList interviewers = interviewerService.findInterviewersByJobPostingId(jobPostingId);
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.success(interviewers));
+    }
+
+    @GetMapping("/jobPosting")
+    public ResponseEntity<BaseResponse<Object>> getInterviewers(@RequestParam(defaultValue = "0") int page,
+                                                              @RequestParam(name = "search", required = false) String keyword) {
+
+        AdminDto.AccountInfiniteList findAccounts = userService.findInfiniteAccounts("면접관", page, keyword);
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.success(findAccounts));
     }
 }
